34f8fc6694ca4a61dc40e011887bd286f2445d23
Erick Baca <eabaca@MacBook.local>
2021-11-11 11:14:43 -0700

    Interrupts

---
+++ Lab_2/build/esp-idf/libsodium/CMakeFiles/__idf_libsodium.dir/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.c.obj.d
@@ -0,0 +1,23 @@
+esp-idf/libsodium/CMakeFiles/__idf_libsodium.dir/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.c.obj: \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.c \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/crypto_scalarmult_curve25519.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/8.4.0/include/stddef.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/export.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/8.4.0/include/stdint.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/stdint.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/machine/_default_types.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/sys/features.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/_newlib_version.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/sys/_intsup.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/sys/_stdint.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/8.4.0/include-fixed/limits.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/lib/gcc/xtensa-esp32-elf/8.4.0/include-fixed/syslimits.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/limits.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/newlib.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/sys/cdefs.h \
+ /Users/eabaca/.espressif/tools/xtensa-esp32-elf/esp-2021r1-8.4.0/xtensa-esp32-elf/xtensa-esp32-elf/sys-include/sys/syslimits.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/private/implementations.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_scalarmult/curve25519/scalarmult_curve25519.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/include/sodium/runtime.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_scalarmult/curve25519/ref10/x25519_ref10.h \
+ /Users/eabaca/esp/esp-idf/components/libsodium/libsodium/src/libsodium/crypto_scalarmult/curve25519/ref10/../scalarmult_curve25519.h
--- Lab_2/main/main.c
+++ Lab_2/main/main.c
@@ -6,41 +6,58 @@
 #include "driver/gpio.h"
 
 #define ESP_INTR_FLAG_DEFAULT 0 
+#define LED1 5
+#define LED2 2
+#define GPIO_OUTPUT_PIN_SEL (1ULL << LED2 | 1ULL << LED1)
+#define BUTTON1 23
+#define BUTTON2 22
+#define GPIO_INPUT_PIN_SEL (1ULL << BUTTON1 | 1ULL << BUTTON2)
+
 static xQueueHandle gpio_queue = NULL;
 
+/*Interrupt*/
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
-    GPIO.out ^= BIT0;
+    /*Toggle LEDs*/
+    GPIO.out ^= BIT2;   
+    GPIO.out ^= BIT5;
     uint32_t gpio_num = (uint32_t) arg; 
+    xQueueSendFromISR(gpio_queue, (void *)&gpio_num, NULL); //Send interrupt pin number
 }
 static void print_task(void* arg) {
-    uint32_t gpio_num; while(1) {
-printf("GPIO[%d] caused an interrupt\n", gpio_num); }
+    uint32_t gpio_num; 
+    while(1) {
+        if(xQueueReceive(gpio_queue, (void *)&gpio_num, portMAX_DELAY)){
+            printf("GPIO[%d] caused an interrupt\n", gpio_num); //Print ISR pin number
+        } 
+    }
 }
 void setUpGPIO() {
     gpio_config_t io_conf;
-
     //INPUT     
-    io_conf.intr_type = ; 
-    io_conf.mode = ; 
-    io_conf.pin_bit_mask = ; 
-    io_conf.pull_down_en = ; 
-    io_conf.pull_up_en = ; 
+    io_conf.intr_type = GPIO_INTR_POSEDGE;  //Set up as Positive Edge
+    io_conf.mode = GPIO_MODE_INPUT;     //Set pins as input
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;     
+    io_conf.pull_down_en = 1;   //Enable as a pulldown button
+    io_conf.pull_up_en = 0; 
     gpio_config(&io_conf);
 
     //OUTPUT 
-    io_conf.intr_type = ; 
-    io_conf.mode = ; 
-    io_conf.pin_bit_mask = ; 
-    io_conf.pull_down_en = ; 
-    io_conf.pull_up_en = ; 
+    io_conf.intr_type = GPIO_INTR_DISABLE;  //Its an output so the interrupt is disable
+    io_conf.mode = GPIO_MODE_OUTPUT;    //Set pin as output
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL; 
+    io_conf.pull_down_en = 0; 
+    io_conf.pull_up_en = 0; 
     gpio_config(&io_conf);
 
     //Set ISR 
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(xxx, gpio_isr_handler, (void*) xxx); 
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);    //Ste the flag for th einterrupt
+
+    /*Let button 1 and button two to interract with the interrupt*/
+    gpio_isr_handler_add(BUTTON1, gpio_isr_handler, (void*) BUTTON1);   
+    gpio_isr_handler_add(BUTTON2, gpio_isr_handler, (void*) BUTTON2);     
 }
 void app_main() {
     setUpGPIO();
-    gpio_queue = xQueueCreate(10, sizeof(uint32_t)); 
+    gpio_queue = xQueueCreate(10, sizeof(uint32_t));    //Queue to send information
     xTaskCreate(&print_task, "print_task", 2048, NULL, 10, NULL);
 }
